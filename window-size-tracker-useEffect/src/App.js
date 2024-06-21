ac581edf582ea363ccc0c990d518e31e0933db1b
Umka-dev <design.tvin@gmail.com>
2024-06-21 21:33:18 +0200

    add useEffect that changes the background color depending on the screen width

--- window-size-tracker-useEffect/src/App.js
+++ window-size-tracker-useEffect/src/App.js
@@ -1,13 +1,36 @@
 import "../public/styles.css";
-import React from "react";
+import React, { useState, useEffect } from "react";
 import WindowSizeComponent from "./WindowSizeComponent";
 
 function App() {
+  const [bgColor, setBgColor] = useState("");
+
+  // Function to update style depending on window width
+  const updateBgColor = (deviceType) => {
+    const color =
+      deviceType === "mobile"
+        ? "lightblue"
+        : deviceType === "tablet"
+        ? "lightgreen"
+        : deviceType === "pc"
+        ? "lightcoral"
+        : "white";
+    setBgColor(color);
+  };
+
+  // Update body style when bgColor changes
+  useEffect(() => {
+    document.body.style.backgroundColor = bgColor;
+  }, [bgColor]);
+
   return (
+    // <div style={{ backgroundColor: bgColor }}> // using inline styling
     <div className="App">
       <h1>Window Size Tracker</h1>
-      <WindowSizeComponent />
+      <WindowSizeComponent onResize={updateBgColor} />
+      <p>Current background color: {bgColor}</p>
     </div>
+    // </div>
   );
 }
 
--- window-size-tracker-useEffect/src/WindowSizeComponent.jsx
+++ window-size-tracker-useEffect/src/WindowSizeComponent.jsx
@@ -1,26 +1,39 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
-function WindowSizeComponent() {
+function WindowSizeComponent({ onResize }) {
   const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+  const [device, setDevice] = useState();
 
   useEffect(() => {
     // Window resizing handler
     function handleResize() {
-      setWindowWidth(window.innerWidth);
+      const newWidth = window.innerWidth;
+      setWindowWidth(newWidth);
+
+      const deviceType =
+        newWidth <= 600 ? "mobile" : newWidth <= 900 ? "tablet" : "pc";
+      setDevice(deviceType);
+      onResize(deviceType);
     }
 
+    console.log("Current device: " + device);
+
     // Subscribe to a window resize event
-    window.addEventListener('resize', handleResize);
+    window.addEventListener("resize", handleResize);
+
+    // Initialization when the component is first loaded
+    handleResize();
 
     // Cleanup function: unsubscribe from window resize event
     return () => {
-      window.removeEventListener('resize', handleResize);
+      window.removeEventListener("resize", handleResize);
     };
-  }, []); // Empty dependency array, effect executed only once
+  }, [windowWidth, onResize]); // Effect depends on screen size change and props - onResize
 
   return (
     <div>
       <p>Current window width: {windowWidth}px</p>
+      <p>Current device: {device}</p>
     </div>
   );
 }
