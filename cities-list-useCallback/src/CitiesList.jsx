13929c7926beace5fd5935f29bfdd5e2c36fba60
Umka-dev <design.tvin@gmail.com>
2024-06-28 12:47:51 +0200

    bug fixing and add styled component

--- cities-list-useCallback/src/CitiesList.jsx
+++ cities-list-useCallback/src/CitiesList.jsx
@@ -1,10 +1,22 @@
-import React from "react";
-import { City } from "./City";
+import React from 'react';
+import { City } from './City';
+import styled from 'styled-components';
+
+const List = styled.list`
+  padding: 10px;
+  margin: 10px 0;
+  font-size: 3em;
+`;
 
 export const CitiesList = React.memo(({ list, onRemoveClick }) => {
-  console.log("List render");
+  console.log('List render');
 
   return list.map((item) => {
-    return <City key={item} city={item} onRemoveClick={onRemoveClick} />;
+    return (
+      <>
+        <List />
+        <City key={item} city={item} onRemoveClick={onRemoveClick} />
+      </>
+    );
   });
 });
