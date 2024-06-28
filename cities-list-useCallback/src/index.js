c84b46ecf060b442d21add9a970f29e2a6f91976
Umka-dev <design.tvin@gmail.com>
2024-06-28 17:28:25 +0200

    solving dependency compatibility issues

--- cities-list-useCallback/src/index.js
+++ cities-list-useCallback/src/index.js
@@ -1,12 +1,12 @@
+import { render } from 'react-dom';
 import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
 import App from './App';
 
 const rootElement = document.getElementById('root');
-const root = createRoot(rootElement);
 
-root.render(
+render(
   <StrictMode>
     <App />
-  </StrictMode>
+  </StrictMode>,
+  rootElement
 );
