2b7084f5ec919f2970fb032d9f981335c68a4d8c
Umka-dev <design.tvin@gmail.com>
2024-07-02 21:11:44 +0200

    create new app and add code

---
+++ fetch-api-users/src/App.js
@@ -0,0 +1,28 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+import { Container, Typography } from '@mui/material';
+import Users from './Users';
+
+const App = () => {
+  return (
+    <div className='App'>
+      <header className='App-header'>
+        <img
+          src={logo}
+          className='App-logo'
+          alt='logo'
+          sx={{ width: '100px' }}
+        />
+        <Container>
+          <Typography variant='h2' align='center' gutterBottom>
+            Fetch API Example
+          </Typography>
+          <Users />
+        </Container>
+      </header>
+    </div>
+  );
+};
+
+export default App;
